fbe2c70728cca3fe284294e0c52f9f1b253383ba
Dylan Cavanaugh <cavanadm@gmail.com>
2018-12-06 14:04:04 -0600

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class AsyncForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.outputTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calls to Fibonacci(x), Fibonacci(x + 1), and Fibonacci(x + 2) \r\nin separate threa" +
+    "ds";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Number (1 - 100):";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(129, 51);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(51, 20);
+            this.inputTextBox.TabIndex = 2;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(220, 49);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // outputTextbox
+            // 
+            this.outputTextbox.Location = new System.Drawing.Point(16, 81);
+            this.outputTextbox.Multiline = true;
+            this.outputTextbox.Name = "outputTextbox";
+            this.outputTextbox.ReadOnly = true;
+            this.outputTextbox.Size = new System.Drawing.Size(279, 263);
+            this.outputTextbox.TabIndex = 4;
+            // 
+            // AsyncForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 356);
+            this.Controls.Add(this.outputTextbox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AsyncForm";
+            this.Text = "Asynchronous Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox outputTextbox;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,117 @@
+﻿//Program By : Dylan Cavanaugh
+//Working with the Fibonacci Sequence
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class AsyncForm : Form
+    {
+        public AsyncForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void startButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Make sure the input is within bounds
+                if (Convert.ToInt32(inputTextBox.Text) > 0 && Convert.ToInt32(inputTextBox.Text) < 100)
+                {
+                    outputTextbox.Text =
+                       $"Starting Task to calculate Fibonacci({inputTextBox.Text})\r\n";
+
+                    //Create Task to perform Fibonacci(x+1) calculation in a thread
+                    Task<TimeData> task1 = Task.Run(() => StartFibonacci(Convert.ToInt32(inputTextBox.Text)));
+
+
+                    outputTextbox.AppendText(
+                        $"Starting to calculate Fibonacci({(Convert.ToInt32(inputTextBox.Text) + 1)})\r\n");
+
+                    Task<TimeData> task2 = Task.Run(() => StartFibonacci(Convert.ToInt32(inputTextBox.Text) + 1));
+
+                    //create Task to perform Fibonacci(x+2) calculation in a thread
+                    outputTextbox.AppendText(
+                        $"Starting to calculate Fibonacci({(Convert.ToInt32(inputTextBox.Text) + 2)})\r\n");
+
+                    Task<TimeData> task3 = Task.Run(() => StartFibonacci(Convert.ToInt32(inputTextBox.Text) + 2));
+
+
+
+                    TimeData[] times = await Task.WhenAll(task1, task2, task3); //await for all to complete
+                    double totalMins = 0;
+
+                    //Using Array of TimeData from tasks, calculate the total time for three tasks
+                    foreach (var time in times)
+                    {
+                        totalMins += (time.EndTime - time.StartTime).TotalMinutes;
+                    }
+                    outputTextbox.AppendText(
+                        $"Total calculation time = {totalMins:F6} minutes\r\n");
+                }
+                //If input is above 100 or below 1, output MessageBox
+                else
+                {
+                    MessageBox.Show("Number Out Of Bounds, Try Again");
+                }
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Please Enter A Number");
+            }
+
+
+
+            //starts a call to Fibonacci and captures start/end times
+            TimeData StartFibonacci(int n)
+            {
+                //create a TimeData object to store start/end time
+                var result = new TimeData();
+                
+
+                AppendText($"Calculating Fibonacci({n})");
+                result.StartTime = DateTime.Now;
+                long fibonacciValue = Fibonacci(n);
+                result.EndTime = DateTime.Now;
+
+                //Output the Fib number and calculated time for each
+                AppendText($"Fibonacci({n}) = {fibonacciValue} \r\n Calculation time = " +
+                    $"{(result.EndTime - result.StartTime).TotalMinutes:F6} minutes");
+
+                return result;
+
+            }
+        }
+        //Recursively calculates Fibonacci numbers
+        public long Fibonacci(long n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+
+        //append text to outputTextBox in UI thread
+        public void AppendText(String text)
+        {
+            if(InvokeRequired)//not GUI thread, so add to GUI thread
+            {
+                Invoke(new MethodInvoker(() => AppendText(text)));
+            }
+            else //GUI thread so append text
+            {
+                outputTextbox.AppendText(text + "\r\n");
+            }
+        }
+    }
+}
